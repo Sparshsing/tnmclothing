84ee2606f98a3ae5c876b9b5b95c2aff8c472408
Sparsh Singhal <sparshsing96@gmail.com>
2021-05-12 19:19:55 +0530

    order and migrations

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Order
+from inventory.models import Inventory
 
 class OrderSerializer(serializers.ModelSerializer):
     # orderCount = serializers.SerializerMethodField()
@@ -7,10 +8,19 @@ class OrderSerializer(serializers.ModelSerializer):
     # productAvailability = serializers.SerializerMethodField()
     # orderStatus = serializers.SerializerMethodField()
     # style = serializers.SerializerMethodField()
+    productAvailability = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
         fields = ('orderId', 'store', 'storeName', 'orderStatus', 'saleDate', 'orderNo', 'orderCount', 'recipientName', 'style', 'size', 'color', 'design', 'processing', 'printed', 'shipped', 'sfmNotes', 'buyerName', 'buyerEmail', 'buyerComments', 'giftMessages', 'sfmId', 'sku', 'shipDate', 'priorityShip', 'customerPaidShipping', 'trackingNumber', 'productAvailability')
+        read_only_fields = ['productAvailability']
+
+    def get_productAvailability(self, order):
+        res = Inventory.objects.filter(sfmId=order.sfmId).first()
+        if res:
+            return res.productAvailability
+        else:
+            return "Unable to fetch"
 
     # def get_sfmId(self, order):
     #     return order.style + '-' + order.size + '-' + order.color
@@ -18,8 +28,7 @@ class OrderSerializer(serializers.ModelSerializer):
     # def get_orderCount(self, order):
     #     return Order.objects.filter(orderNo=order.orderNo).count()
 
-    # def get_productAvailability(self, order):
-    #     return 'dummy'
+
     #
     # def get_orderStatus(self, order):
     #     if order.shipped.upper() == 'Y':
