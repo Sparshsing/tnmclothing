54c5139522cd30d0aa88a03f3692d2797456e4e6
Sparsh Singhal <sparshsing96@gmail.com>
2021-07-22 13:00:14 +0530

    wsgi and inventory list

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -8,3 +8,21 @@ class InventorySerializer(serializers.ModelSerializer):
         fields = ('id', 'sfmId', 'style', 'size', 'color', 'inStock', 'arrivalDate', 'minimum', 'maximum', 'inTransit', 'unfulfilledCount', 'productAvailability', 'trueCount', 'shortCount', 'needToPurchase')
         # extra_kwargs = {'productAvailability': {'read_only': True, 'default': 'not bad'}
         #                 }
+
+class InventoryListSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    sfmId = serializers.CharField()
+    style = serializers.CharField()
+    size = serializers.CharField()
+    color = serializers.CharField()
+    inStock = serializers.IntegerField()
+    arrivalDate = serializers.DateField()
+    minimum = serializers.IntegerField()
+    maximum = serializers.IntegerField()
+    inTransit = serializers.IntegerField()
+    unfulfilledCount = serializers.IntegerField()
+    # comment these calculated fields for performance, calculate in client
+    # productAvailability = serializers.CharField()
+    # trueCount = serializers.IntegerField()
+    # shortCount = serializers.IntegerField()
+    # needToPurchase = serializers.IntegerField()
